b936815f9cb61b72d8c8e945826d6a893dd8835c
Latif Maloku <latifmaloku@@my.unt.edu>
2025-08-16 23:43:23 -0500

    move first row into loop, and reindex

--- Easy/pascalsTriangle.cpp
+++ Easy/pascalsTriangle.cpp
@@ -13,8 +13,7 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> returnVect;
-        returnVect.push_back({1});
-        for(int i = 1; i < numRows; i++)
+        for(int i = 0; i < numRows; i++)
         {
             vector<int> row;
             for (int j = 0; j < i+1; j++)
