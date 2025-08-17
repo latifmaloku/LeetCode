b29a8cf1fc6e301f0efca74343b9b7dddcb154dc
Latif Maloku <latifmaloku@@my.unt.edu>
2025-08-16 23:43:45 -0500

    added comments to pascalsTriangle solution

--- Easy/pascalsTriangle.cpp
+++ Easy/pascalsTriangle.cpp
@@ -6,7 +6,13 @@ In Pascal's triangle, each number is the sum of the two numbers directly above i
 
 /*
 * first and last element is always going to be one for each row
-
+* for i 0...numRows
+* for j 0...numCels=i+1
+* 1. if first cell or last then insert one into row
+* 2. else add  cell = previousRow[j-1]+previousRow[j] to current row
+* end loop
+* end loop
+* return solution
 */
 
 class Solution {
@@ -15,23 +21,28 @@ public:
         vector<vector<int>> returnVect;
         for(int i = 0; i < numRows; i++)
         {
+            // store row i
             vector<int> row;
             for (int j = 0; j < i+1; j++)
             {
+                // is this first or last cell in current row
                 if(j == 0 || j == i)
                 {
                     row.push_back(1);
                 }
                 else
                 {
+                    // cell = previousRow[j-1]+previousRow[j] to current row
                     row.push_back(returnVect[i-1][j-1]+returnVect[i-1][j]);
                 }
 
             }
+            // add current row to solution
             returnVect.push_back(row);
 
 
         }
+        // return solution
         return returnVect;
     }
 };
